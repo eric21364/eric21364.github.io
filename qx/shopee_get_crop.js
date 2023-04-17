ad8605642a1219c36d5318427b750ac9de0b283c
eric21364 <eric21364@gmail.com>
2023-04-17 10:20:12 +0800

    Update shopee_get_crop.js

--- qx/shopee_get_crop.js
+++ qx/shopee_get_crop.js
@@ -34,6 +34,8 @@ function surgeNotify(subtitle = '', message = '') {
   async function getCropData() {
     return new Promise((resolve, reject) => {
       try {
+        console.log($request)
+        console.log($task)
         const body = JSON.parse($request.body);
         if (body && body.cropId && body.resourceId && body.s) {
           let shopeeFarmInfo = getSaveObject('ShopeeFarmInfo');
