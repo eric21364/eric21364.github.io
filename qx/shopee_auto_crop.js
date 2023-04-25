7d166362bbc7e64db084ba6edf8bf7681652bc0e
eric21364 <eric21364@gmail.com>
2023-04-25 09:04:34 +0800

    Update shopee_auto_crop.js

--- qx/shopee_auto_crop.js
+++ qx/shopee_auto_crop.js
@@ -106,7 +106,6 @@ async function getSeedList() {
                         let found = false;
                         let haveSeed = true;
                         for (const cropName of config.autoCropSeedNames) {
-                            console.log(`🔍尋找「${cropName}」種子`);
                             for (const crop of cropMetas) {
                                 // console.log(`🔍 找到「${crop.name}」種子`);
                                 if (crop.name.includes(cropName)) {
@@ -164,17 +163,18 @@ async function createCrop() {
 
             $task.fetch(createCropRequest).then(response => {
                 const data = response.body
+                console.log(data)
                 if (response.statusCode == 200) {
                     const obj = JSON.parse(data);
                     if (obj.msg === 'success') {
                         const cropId = obj.data.crop.id;
-                        let shopeeCrop = JSON.parse($prefs.valueForKey('ShopeeCrop'));
+                        let shopeeCrop = JSON.parse($prefs.valueForKey('ShopeeFarmInfo'));
                         if (shopeeCrop) {
                             shopeeCrop.cropId = cropId;
                         } else {
                             shopeeCrop = { 'cropId': cropId };
                         }
-                        const saveShopeeCrop = $prefs.setValueForKey(JSON.stringify(shopeeCrop), 'ShopeeCrop');
+                        const saveShopeeCrop = $prefs.setValueForKey(JSON.stringify(shopeeCrop), 'ShopeeFarmInfo');
                         return resolve();
                     } else if (obj.code === 409003) {
                         return reject(['自動種植失敗 ‼️', `目前有正在種的作物「${obj.data.crop.meta.name}」`]);
@@ -188,8 +188,6 @@ async function createCrop() {
                 }
             }).catch(error => {
                 if (error) {
-                    console.log(error)
-               
                     return reject(['自動種植失敗 ‼️', '連線錯誤']);
                 }
             })
