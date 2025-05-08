154a16672732a2a5644d34352de59edcba3daced
eric21364 <eric21364@gmail.com>
2025-05-08 16:11:31 +0800

    Update shopee_auto_water.js

--- qx/shopee_auto_water.js
+++ qx/shopee_auto_water.js
@@ -51,6 +51,7 @@ async function preCheck() {
         const shopeeHeaders = {
             'Cookie': cookieToString(shopeeInfo.token),
             'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Mobile Safari/537.36'
         }
         config = {
             shopeeInfo: shopeeInfo,
